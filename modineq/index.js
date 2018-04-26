039df0ad24421d735ed1a07d565669d1f36b1f0d
ashiato45 <katiiijp@gmail.com>
2018-04-26 23:05:41 +0900

    changed constarints in modineq

--- modineq/index.js
+++ modineq/index.js
@@ -18,6 +18,8 @@ function draw() {
     var b = parseInt(document.getElementById("txtB").value);
     var c = parseInt(document.getElementById("txtC").value);
     var m = parseInt(document.getElementById("txtM").value);
+    var d = parseInt(document.getElementById("txtD").value);
+    var e = parseInt(document.getElementById("txtE").value);
     var sp = size / m;
     for (var i = 0; i < m; i++) {
         for (var j = 0; j < m; j++) {
@@ -33,11 +35,18 @@ function draw() {
     }
     for (var i = 0; i < m; i++) {
         for (var j = 0; j < m; j++) {
-            if (mod(a * i + b * j, m) <= mod(c, m)) {
+            var temp = mod(a * i + b * j + e, m);
+            if (temp <= mod(c, m)) {
                 ctx.beginPath();
                 var p = getPos(sp, i, j);
                 ctx.arc(p.x, p.y, sp / 3, 0, 2 * Math.PI, false);
-                ctx.fillStyle = "Red";
+                console.log(a * i + b * j + e, 0 <= a * i + b * j + e, a * i + b * j + e < m, 0 <= c, c < m, 0 <= d, d < m);
+                if (0 <= a * i + b * j + e && a * i + b * j < m && 0 <= c && c < m && 0 <= d && d < m) {
+                    ctx.fillStyle = "Blue";
+                }
+                else {
+                    ctx.fillStyle = "Red";
+                }
                 ctx.fill();
             }
         }
